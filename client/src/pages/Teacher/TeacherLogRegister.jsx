46894c7464998625e102cd2ba74a251667963962
studpeps <43947328+studpeps@users.noreply.github.com>
2020-05-25 02:28:43 +0530

    change-student

--- client/src/pages/Teacher/TeacherLogRegister.jsx
+++ client/src/pages/Teacher/TeacherLogRegister.jsx
@@ -15,6 +15,7 @@ export default function TeacherLogin() {
     const [signUpEmail,setSignUpEmail] = useState("")
     const [signUpPassword, setSignUpPassword] = useState("")
     const [mossId, setMossId] = useState("")
+
     
 
     const signIn = e =>{
@@ -60,13 +61,16 @@ export default function TeacherLogin() {
     return <div>
     <div className="teacherLogRegister_cont">
       <div className="teacherLogRegister_form sign-in" >
-        <h2 className="teacherLogRegister_Landing-center">Welcome back</h2>
+        <h2 className="teacherLogRegister_Landing-center">Welcome back,</h2>
         <label className="teacherLogRegister_label">
-          <input className="teacherLogRegister_input" type="email" placeholder="Email Address" value={loginEmail} onChange={e=>setLoginEmail(e.target.value)}/>
+          <span className="teacherLogRegister_span">Email</span>
+          <input className="teacherLogRegister_input" type="email"  value={loginEmail} onChange={e=>setLoginEmail(e.target.value)}/>
         </label>
         <label className="teacherLogRegister_label">
-          <input className="teacherLogRegister_input" type="password" placeholder="Password" value={loginPassword} onChange={e=>setLoginPassword(e.target.value)}/>
+          <span className="teacherLogRegister_span">Password</span>
+          <input className="teacherLogRegister_input" type="password"  value={loginPassword} onChange={e=>setLoginPassword(e.target.value)}/>
         </label>
+        <p className="teacherLogRegister_forgot-pass"><a href="#">Forgot password?</a></p>
         <button onClick={signIn} type="button" className="teacherLogRegister_submit">Sign In</button>
       </div>
       <div className="teacherLogRegister_sub-cont">
@@ -85,18 +89,22 @@ export default function TeacherLogin() {
           </div>
         </div>
         <div className="teacherLogRegister_form sign-up">
-          <h2 className="teacherLogRegister_h2">Greetings, Stranger</h2>
+          <h2 className="teacherLogRegister_h2">Time to feel like home,</h2>
           <label className="teacherLogRegister_label">
-            <input className="teacherLogRegister_input" placeholder="NAME" value={name} onChange={e=>setName(e.target.value)}type="text" />
+            <span className="teacherLogRegister_span">Name</span>
+            <input className="teacherLogRegister_input"  value={name} onChange={e=>setName(e.target.value)}type="text" />
           </label>
           <label className="teacherLogRegister_label">
-            <input className="teacherLogRegister_input" placeholder="EMAIL" value={signUpEmail} onChange={e=>setSignUpEmail(e.target.value)}type="email" />
+            <span className="teacherLogRegister_span">Email</span>
+            <input className="teacherLogRegister_input"  value={signUpEmail} onChange={e=>setSignUpEmail(e.target.value)}type="email" />
           </label>
           <label className="teacherLogRegister_label">
-            <input className="teacherLogRegister_input" type="text" placeholder="MOSS ID" value={mossId} onChange={e=>setMossId(e.target.value)}/>
+          <span className="teacherLogRegister_span">MOSS-ID</span>
+            <input className="teacherLogRegister_input" type="text" value={mossId} onChange={e=>setMossId(e.target.value)}/>
           </label>
           <label className="teacherLogRegister_label">
-            <input className="teacherLogRegister_input" type="password" placeholder="PASSWORD" value={signUpPassword} onChange={e=>setSignUpPassword(e.target.value)} />
+            <span className="teacherLogRegister_span">Password</span>
+            <input className="teacherLogRegister_input" type="password"  value={signUpPassword} onChange={e=>setSignUpPassword(e.target.value)} />
           </label>
           <button onClick={registerUser}type="button" className="teacherLogRegister_submit">Sign Up</button>
 
@@ -106,3 +114,5 @@ export default function TeacherLogin() {
   </div>
 
     }
+
+
