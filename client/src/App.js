2d21f0e916534cb5b49e07786951ba883e6c6a7c
studpeps <43947328+studpeps@users.noreply.github.com>
2020-05-25 20:48:14 +0530

    student

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,10 @@ import React from 'react';
 import {BrowserRouter,Route} from 'react-router-dom';
 
 import {AuthProvider} from './context/Auth'
-import TeacherLogRegister from "./pages/Teacher/TeacherLogRegister"
 import "./App.css"
 import LandingPage from './pages/LandingPage/LandingPage';
-import PrivateTeacherRoute from './components/PrivateTeacherRoute';
-<<<<<<< HEAD
-import PrivateStudentRoute from './components/PrivateStudentRoute';
-=======
->>>>>>> 464982139b803c14fd4cbbd55b9ba728dd7010c1
+
+
 
 function App() {
   return (
