cda49dd3310d7388dd20726b59bc0e3d10d18864
Kushal Garg <kushalgarg2000@gmail.com>
2020-05-25 13:09:27 +0530

    Adds route to add student

--- api/routes/adminRoutes.js
+++ api/routes/adminRoutes.js
@@ -3,11 +3,11 @@ const Authcontroller = require('../controllers/AuthController')
 
 const router = express.Router()
 
-const {CheckAuthentication, createTeacher, checkRole} = Authcontroller;
+const {CheckAuthentication, createTeacher, createStudent, checkRole} = Authcontroller;
 
 router.post('/role',checkRole)
 
 router.post("/teacher/create",CheckAuthentication,createTeacher)
-
+router.post("/student/create",CheckAuthentication,createStudent)
 
 module.exports = router
