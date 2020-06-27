ca22bc9732c4f7728556744b20236ced7770ef62
Kushal Garg <kushalgarg2000@gmail.com>
2020-06-28 01:17:59 +0530

    Adds create and delete routes for course level assignments

--- api/routes/teacherRoutes.js
+++ api/routes/teacherRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const multer = require('multer')
+
 const TeacherController = require('../controllers/TeacherController')
 const {teacherAuthentication} = require('../controllers/AuthController')
+
 const router = express.Router()
+const storage = multer.memoryStorage()
+const upload = multer({storage:storage})
 
 router.post('/course',teacherAuthentication,TeacherController.createCourse)
 router.post('/course/read',teacherAuthentication,TeacherController.viewCourse)
@@ -12,5 +17,8 @@ router.post('/course/class',teacherAuthentication,TeacherController.createClass)
 router.post('/course/class/read',teacherAuthentication,TeacherController.viewClassOfCourse)
 router.post('/course/class/delete',teacherAuthentication,TeacherController.deleteClass)
 router.post('/course/class/update',teacherAuthentication,TeacherController.editClass)
+
+router.post('/course/assignment',teacherAuthentication,upload.single('attachment'),TeacherController.createCourseAssignment)
+router.post('/course/assignment/delete',teacherAuthentication,TeacherController.deleteCourseAssignment)
 
 module.exports = router
