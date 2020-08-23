ff754c83a582f64c997d9f87f1458c51c8cd9aa8
Stuti Prasad <stuti.prasad2k@gmail.com>
2020-08-23 13:33:17 +0530

    assignments

---
+++ client/src/pages/Teacher/TeacherAssignment/TeacherAssignmentCard.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { TrashIcon,EditIcon} from 'react-line-awesome'
+import "./TeacherAssignmentCard.css"
+function TeacherAssignment(props){
+var randomColor=require('randomcolor');
+
+function DeleteAssignment(){
+        
+    props.user.getIdToken()
+.then(token=>{
+    fetch(`${process.env.REACT_APP_URL}/teacher/course/class/delete`,{
+        method:"post",
+        headers:{
+            "Authorization" : `Bearer ${token}`,
+            "Content-type":"application/json"
+        },
+            })
+    .then(response=>response.json())
+    .then(body=>{console.log(body);
+    props.ViewCourseAssignment(props.uniqueCourseId)})
+})
+
+}
+
+return <div style={{backgroundColor:randomColor({luminosity:'light'})}} className="assignmentCard">
+   <div className="assignmentHead"> <h2>{props.assignmentName}</h2></div>
+   <div className="assignmentSubHead">
+   {props.assignmentDescription}
+   <hr></hr>
+        {props.dueDate}
+    {props.assignmentLanguage}
+    </div>
+<TrashIcon onClick={DeleteAssignment}/>
+</div>
+}
+
+export default TeacherAssignment;
--- client/src/pages/Teacher/TeacherClass/TeacherClassPage.jsx
+++ client/src/pages/Teacher/TeacherClass/TeacherClassPage.jsx
@@ -3,13 +3,41 @@ import TeacherPanel from "../TeacherPanel/TeacherPanel"
 import TeacherClassCard from "./TeacherClassCard"
 import TeacherCreateClass from "./TeacherCreateClass";
 import TeacherCourseAssignment from "./TeacherCourseAssignment"
+import TeacherAssignmentCard from "../TeacherAssignment/TeacherAssignmentCard"
+import "./TeacherClassPage.css"
 import {PlusIcon} from 'react-line-awesome'
 function TeacherClassPage(props){
 
     const [showCourseAssignment,setShowCourseAssignment]=useState(false);
+    const [courseAssignment,setCourseAssignment]=useState([]);
     const [showCreateClasses,setShowCreateClasses]=useState(false);
     const [classes,setClasses]=useState([]);
     
+    function ViewCourseAssignment(courseId){
+        setCourseAssignment([])
+        props.user.getIdToken()
+        .then(token=>{
+            console.log(token)
+            fetch(`${process.env.REACT_APP_URL}/teacher/course/assignment/read`,{
+                method:"post",
+                headers:{
+                    "Authorization" : `Bearer ${token}`,
+                    "Content-type":"application/json"
+                },
+                body:JSON.stringify(
+                  {
+                  courseId:courseId
+                  }
+                )
+            })
+            .then(response=>response.json())
+            .then(body=>{
+                console.log(body)
+                setCourseAssignment(body.data.courseAssignments)
+            })
+        })
+    }
+    useEffect(()=>{ViewCourseAssignment(props.location.state.uniqueCourseId)},[])
 
     function ViewClass(courseId){
         setClasses([])
@@ -41,12 +69,17 @@ function TeacherClassPage(props){
     return <div className="ClassPage">
     <TeacherPanel user={props.user}/>
     <div className="courseAssignment">
-    <h2><p>ASSIGNMENTS</p></h2>
+    <div className="classButton"><h2><p>ASSIGNMENTS</p></h2>
     <button className="teacherCard_createClass" onClick={()=>setShowCourseAssignment(true)} ><PlusIcon/> Create Course Assignment</button>
+    </div>
+    <div className="assignments">
     {
-     showCourseAssignment && <TeacherCourseAssignment offModal={()=>setShowCourseAssignment(false)} user={props.user} uniqueCourseId={props.location.state.uniqueCourseId} />
+     showCourseAssignment && <TeacherCourseAssignment offModal={()=>setShowCourseAssignment(false)} user={props.user} ViewCourseAssignment={ViewCourseAssignment} uniqueCourseId={props.location.state.uniqueCourseId} />
     }
-    <div>jhdfsdfi</div>
+        {
+           courseAssignment.map(assignObj=><TeacherAssignmentCard user={props.user} assignmentDescription={assignObj.description} ViewCourseAssignment={ViewCourseAssignment} uniqueCourseId={props.location.state.uniqueCourseId} assignmentLanguage={assignObj.language} assignmentName={assignObj.name} dueDate={assignObj.dueDate.seconds}/>)
+        }
+    </div>
     </div>
     <div className="Classes">
     <div className="classButton">
