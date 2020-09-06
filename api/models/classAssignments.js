8f493ed06c59b807a26c04a07d32cc3f19b82771
Kushal Garg <kushalgarg2000@gmail.com>
2020-09-06 22:11:52 +0530

    Adds delete/read routes for class assignments

--- api/models/classAssignments.js
+++ api/models/classAssignments.js
@@ -60,3 +60,61 @@ module.exports.createAssignment = async ({
 		throw new Error('Unauthorized to add assignment to this course');
 	}
 };
+
+module.exports.getAssignments = async ({ uid, courseId, classId }) => {
+	const docRef = courseDb.doc(courseId).collection('classes').doc(classId);
+	const doc = await docRef.get();
+	if (doc.data().classCoordinator === uid) {
+		const querySnapshot = await docRef.collection('assignments').get();
+
+		const assignments = [];
+		const fileURLPromises = [];
+		querySnapshot.forEach(document => {
+			const { attachment, ...assignmentData } = document.data();
+
+			if (!document.data().isDeleted) {
+				const options = {
+					version: 'v4',
+					action: 'read',
+					expires: Date.now() + 60 * 60 * 1000, //expires in one hour
+				};
+
+				fileURLPromises.push(bucket.file(attachment.gcs_fileName).getSignedUrl(options));
+
+				assignments.push({
+					...assignmentData,
+					assignmentId: document.id,
+					attachment: {
+						mimetype: attachment.mimetype,
+						originalname: attachment.originalname,
+					},
+				});
+			}
+		});
+		const fileURLs = await Promise.all(fileURLPromises);
+		return assignments.map((assignment, index) => {
+			assignment.attachment.fileURL = fileURLs[index][0];
+			return assignment;
+		});
+	} else {
+		throw new Error('Unauthorized to access this course');
+	}
+};
+
+module.exports.deleteAssignment = async ({ uid, courseId, classId, assignmentId }) => {
+	const docRef = courseDb.doc(courseId).collection('classes').doc(classId);
+	const doc = await docRef.get();
+	console.log(chalk.yellow('Checking if teacher authorized to access course...'));
+	if (doc.data().classCoordinator == uid) {
+		console.log(chalk.green('Teacher authorized!'));
+		console.log(chalk.yellow('Deleting assignment...'));
+		const result = await docRef
+			.collection('assignments')
+			.doc(assignmentId)
+			.update({ isDeleted: true });
+		console.log(chalk.green('Deleted course!'));
+		return;
+	} else {
+		throw new Error('Unauthorized to delete assignment from this course');
+	}
+};
