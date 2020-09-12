e11aa14fa5becd1dfd49455f71555b2a7b3602db
Kushal Garg <kushalgarg2000@gmail.com>
2020-09-13 02:10:10 +0530

    Fixes useDebounce bug

--- client/src/pages/Teacher/TeacherClass/TeacherClassPage.jsx
+++ client/src/pages/Teacher/TeacherClass/TeacherClassPage.jsx
@@ -4,13 +4,14 @@ import TeacherClassCard from './TeacherClassCard';
 import TeacherCreateClass from './TeacherCreateClass';
 import TeacherCourseAssignment from '../TeacherAssignment/TeacherCourseAssignment';
 import TeacherCourseAssignmentCard from '../TeacherAssignment/TeacherCourseAssignmentCard';
-import AddTeacher from "./AddTeacher";
+import AddTeacher from './AddTeacher';
 import './TeacherClassPage.css';
-import  useDebounce from './use-debounce';
+import useDebounce from '../../../hooks/useDebounce';
+
 import { SearchIcon, PlusIcon } from 'react-line-awesome';
 function TeacherClassPage(props) {
-	const [teacherId,setTeacherId]=useState("");
-	const [addTeacherForm,setAddTeacherForm]=useState(false);
+	const [teacherId, setTeacherId] = useState('');
+	const [addTeacherForm, setAddTeacherForm] = useState(false);
 	const [showCourseAssignment, setShowCourseAssignment] = useState(false);
 	const [courseAssignment, setCourseAssignment] = useState([]);
 	const [showCreateClasses, setShowCreateClasses] = useState(false);
@@ -21,6 +22,7 @@ function TeacherClassPage(props) {
 	const [searchResults, setSearchResults] = useState([]);
 
 	useEffect(() => {
+		console.log('Called effect');
 		if (debouncedSearchTerm) {
 			setIsSearching(true);
 			//api call
@@ -50,7 +52,6 @@ function TeacherClassPage(props) {
 		const body = await response.json();
 		return body.data.teachers;
 	};
-  
 
 	function ViewCourseAssignment(courseId) {
 		setCourseAssignment([]);
@@ -112,27 +113,33 @@ function TeacherClassPage(props) {
 						<p>Add a Course Teacher</p>
 					</h2>
 					<div className="searchResultsBox">
-					<input
-						type="text"
-						name="search"
-						placeholder="Search.."
-						onChange={e => setSearchTerm(e.target.value)}
-					/>
-					 {isSearching && <div>Searching ...</div>}
-					 {searchResults.map(result=>
-						 <div style={{
-							display: 'block',
-							width: '200px',
-							margin: '10px'
-						  }}
-						  onClick={()=>setAddTeacherForm(true)}>
-						 <h4>{result.name}</h4>
-						 </div>)}
-						 </div>
-						{addTeacherForm &&
-						<AddTeacher user={props.user} 
-						offModal={()=>setAddTeacherForm(false)} 
-						uniqueCourseId={props.location.state.uniqueCourseId}/>}
+						<input
+							type="text"
+							name="search"
+							placeholder="Search.."
+							onChange={e => setSearchTerm(e.target.value)}
+						/>
+						{isSearching && <div>Searching ...</div>}
+						{searchResults.map(result => (
+							<div
+								style={{
+									display: 'block',
+									width: '200px',
+									margin: '10px',
+								}}
+								onClick={() => setAddTeacherForm(true)}
+							>
+								<h4>{result.name}</h4>
+							</div>
+						))}
+					</div>
+					{addTeacherForm && (
+						<AddTeacher
+							user={props.user}
+							offModal={() => setAddTeacherForm(false)}
+							uniqueCourseId={props.location.state.uniqueCourseId}
+						/>
+					)}
 				</div>
 				<div className="classButton">
 					<h2>
