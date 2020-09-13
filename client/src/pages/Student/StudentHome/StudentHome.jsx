7689b392501009000c0be4f75cf3b6053f394ef5
Stuti Prasad <stuti.prasad2k@gmail.com>
2020-09-13 17:45:14 +0530

    student class

--- client/src/pages/Student/StudentHome/StudentHome.jsx
+++ client/src/pages/Student/StudentHome/StudentHome.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import {PlusIcon} from 'react-line-awesome'
 import StudentClass from "./StudentClass";
 import "./StudentHome.css"
+import StudentAddClass from "./StudentAddClass"
+import { useState } from "react";
 
-function StudentHome(){
+
+function StudentHome(props){
+
+    const [showAddClass,setShowAddClass]=useState(false);
+
     return <div className="studentHomeContainer">
          <div className="studentPanel_container studentPanel_center">
             <nav className="studentPanel_menu">
@@ -11,11 +17,17 @@ function StudentHome(){
 
                 <div className="studentPanel_menu__right">
                     <ul className="studentPanel_menu__list">
-                    <li className="studentPanel_menu__list-item"><button className="studentPanel_menu__link"><PlusIcon/>Add Class</button></li>
+                    <li className="studentPanel_menu__list-item">
+                        <button onClick={()=>setShowAddClass(true)} className="studentPanel_menu__link"><PlusIcon/>Add Class</button></li>
                     </ul>
                 </div>
             </nav>
          </div>
+       
+         {showAddClass && 
+                <StudentAddClass 
+                offModal={()=>setShowAddClass(false)}
+                user={props.user}/>}
       
       <div className="registeredClass">
           <h2><p>Registered Classes</p></h2>
