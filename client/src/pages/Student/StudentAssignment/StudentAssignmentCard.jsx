f5ccd09df6900fd7d4a87ceef8f19036c35fb886
Stuti Prasad <stuti.prasad2k@gmail.com>
2020-09-14 12:06:00 +0530

    student assignment

--- client/src/pages/Student/StudentAssignment/StudentAssignmentCard.jsx
+++ client/src/pages/Student/StudentAssignment/StudentAssignmentCard.jsx
@@ -1,8 +1,24 @@
 import React from "react"
-
+import "./StudentAssignment.css"
 function StudentAssignmentCard(props){
-    return <div>
-        <h2>{props.name}</h2>
+    var randomColor = require('randomcolor');
+    return <div
+    style={{ backgroundColor: randomColor({ luminosity: 'light' }) }}
+    className="assignmentCard"
+>
+    <div className="assignmentHead">
+        {' '}
+        <h2>{props.assignmentName}</h2>
     </div>
+    <div className="assignmentSubHead">
+       <textarea value={props.assignmentDescription}></textarea>
+        <hr></hr>
+        <div>{props.dueDate}</div>
+        <div>{props.assignmentLanguage}</div>
+        <button type="button">View Files</button>
+        <button type="button">Upload Assignment</button>
+        <button type="submit">Submit</button>
+    </div>
+</div>
 }
 export default StudentAssignmentCard;
