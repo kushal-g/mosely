ca92b8659719f7ec010c10431f107d7dfbd55222
Stuti Prasad <stuti.prasad2k@gmail.com>
2020-10-03 20:13:44 +0530

    linking done

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { AuthProvider } from './context/Auth';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import Courses from './pages/Courses/Courses';
 import LinkDrive from './pages/LinkDrive/LinkDrive';
+import DriveLoading from './pages/DriveLoading/DriveLoading';
 import './App.css';
 import GoogleSignIn from './pages/GoogleSignIn/GoogleSignIn';
 import Test from './pages/Test/Test';
@@ -16,6 +17,7 @@ function App() {
 					<Route exact path="/" component={GoogleSignIn} />
 					<Route exact path="/link" component={LinkDrive} />
 					<PrivateRoute exact path="/courses" component={Courses} />
+					<Route exact path="/loading" component={DriveLoading} />
 					<Route exact path="/test" component={Test} />
 				</div>
 			</BrowserRouter>
---
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Sidebar.css';
+
+export default function Sidebar(props) {
+	return <aside className={props.open ? 'sidebar-active' : ''}>zsxdfcghbjmlk</aside>;
+}
