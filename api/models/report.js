35de072314481cb9bbd6af7f9fe11eae11b3b2b1
Kushal Garg <kushalgarg2000@gmail.com>
2020-10-07 16:55:36 +0530

    Completes incremental updates

--- api/models/report.js
+++ api/models/report.js
@@ -2,9 +2,20 @@ const db = require('../utils/base').firestore();
 
 const reportDb = db.collection('reports');
 
-module.exports.save = async (courseWorkId, report) => {
+module.exports.save = async ({ courseWorkId, report, lastUpdateTime, numSubmissions }) => {
 	await reportDb.doc(courseWorkId).set({
 		report,
+		numSubmissions,
+		lastUpdateTime,
 		noneMatching: report.length === 0,
 	});
+};
+
+module.exports.get = async courseWorkId => {
+	const doc = await reportDb.doc(courseWorkId).get();
+	return doc.data();
+};
+
+module.exports.delete = async courseWorkId => {
+	await reportDb.doc(courseWorkId).delete();
 };
