2577917ad1b134141f27bfd9e150f0b2804fdf3d
Stuti Prasad <stuti.prasad2k@gmail.com>
2020-10-08 01:28:45 +0530

    assignment

--- client/src/pages/CourseAssignment/CourseAssignment.jsx
+++ client/src/pages/CourseAssignment/CourseAssignment.jsx
@@ -1,17 +1,50 @@
 import React, { useContext, useEffect, useState } from 'react';
+import useLocalStorage from '../../Hooks/useLocalStorage';
+import { useParams } from 'react-router-dom';
 import './CourseAssignment.css';
 import { AuthContext } from '../../context/Auth';
 import CourseAssignmentCard from './components/CourseAssignmentCard';
 import Navbar from '../../components/Navbar/Navbar';
 
+var randomColor = require('randomcolor');
 export default function CourseAssignment(props) {
 	const { currentUser, loading } = useContext(AuthContext);
 	const [courseAssignment, setCourseAssignment] = useState([]);
+	const [course, setCourse] = useState(false);
+
+	const [cardColor, setCardColor] = useLocalStorage(
+		props.location.state.course.id,
+		randomColor({
+			luminosity: 'dark',
+			format: 'rgba',
+			alpha: 0.75,
+		})
+	);
+
+	let { id } = useParams();
+	async function getSingleCourse() {
+		const token = await currentUser.getIdToken();
+		const result = await fetch(
+			`${process.env.REACT_APP_URL}classroom/course/read/?courseId=${id}`,
+			{
+				method: 'get',
+				headers: {
+					Authorization: `Bearer ${token}`,
+					'Content-type': 'application/json',
+				},
+			}
+		);
+
+		const body = await result.json();
+		console.log(body);
+		setCourse(body.data.course);
+		//console.log(body.data);
+	}
 
 	async function getAssignment() {
 		const token = await currentUser.getIdToken();
 		const result = await fetch(
-			`${process.env.REACT_APP_URL}/classroom/courses/courseWork?courseId=${props.location.state.course.id}`,
+			`${process.env.REACT_APP_URL}/classroom/courses/courseWork?courseId=${id}`,
 			{
 				method: 'get',
 				headers: {
@@ -28,33 +61,31 @@ export default function CourseAssignment(props) {
 
 	useEffect(() => {
 		getAssignment();
+		if (props.location.state) setCourse(props.location.state.course);
+		else getSingleCourse();
 	}, [currentUser]);
 
 	return (
 		<div>
 			<Navbar />
-			<div className="assignmentBox">
-				<div className="courseDesc">
-					<div className="assignmentCourseDetails">
-						<div className="assignmentCourseName">
-							{props.location.state.course.name}
-						</div>
-						<div className="assignmentCourseSection">
-							{props.location.state.course.section
-								? props.location.state.course.section
-								: ' '}
-						</div>
-						<div className="courseTeacher">
-							{props.location.state.course.teacherInfo.name.fullName}
+			{course && (
+				<div className="assignmentBox">
+					<div className="courseDesc" style={{ backgroundColor: cardColor }}>
+						<div className="assignmentCourseDetails">
+							<div className="assignmentCourseName">{course.name}</div>
+							<div className="assignmentCourseSection">
+								{course.section ? course.section : ' '}
+							</div>
+							<div className="courseTeacher">{course.teacherInfo.name.fullName}</div>
 						</div>
 					</div>
+					<div className="assignmentCard">
+						{courseAssignment.map(assign => {
+							return <CourseAssignmentCard assignment={assign} />;
+						})}
+					</div>
 				</div>
-				<div className="assignmentCard">
-					{courseAssignment.map(assign => {
-						return <CourseAssignmentCard assignment={assign} />;
-					})}
-				</div>
-			</div>
+			)}
 		</div>
 	);
 }
