c4fd2f3f2ff070d7c4f5514ae496ddab3bd3b81c
Stuti Prasad <stuti.prasad2k@gmail.com>
2020-10-10 17:52:05 +0530

    moss issue

--- client/src/pages/CourseAssignment/CourseAssignment.jsx
+++ client/src/pages/CourseAssignment/CourseAssignment.jsx
@@ -1,91 +1,116 @@
-import React, { useContext, useEffect, useState } from 'react';
-import useLocalStorage from '../../Hooks/useLocalStorage';
-import { useParams } from 'react-router-dom';
-import './CourseAssignment.css';
-import { AuthContext } from '../../context/Auth';
-import CourseAssignmentCard from './components/CourseAssignmentCard';
-import Navbar from '../../components/Navbar/Navbar';
+import React, { useContext, useEffect, useState } from "react";
+import useLocalStorage from "../../Hooks/useLocalStorage";
+import { useParams } from "react-router-dom";
+import "./CourseAssignment.css";
+import { AuthContext } from "../../context/Auth";
+import CourseAssignmentCard from "./components/CourseAssignmentCard";
+import Navbar from "../../components/Navbar/Navbar";
+import AddMossId from "./components/AddMossId";
+import { BorderStyleIcon } from "react-line-awesome";
 
-var randomColor = require('randomcolor');
+var randomColor = require("randomcolor");
 export default function CourseAssignment(props) {
-	const { currentUser, loading } = useContext(AuthContext);
-	const [courseAssignment, setCourseAssignment] = useState([]);
-	const [course, setCourse] = useState(false);
-	let { id } = useParams();
+  const { currentUser, loading } = useContext(AuthContext);
+  const [courseAssignment, setCourseAssignment] = useState([]);
+  const [course, setCourse] = useState(false);
+  const [mossId, setMossId] = useState(false);
+  let { id } = useParams();
 
-	const [cardColor, setCardColor] = useLocalStorage(
-		id,
-		randomColor({
-			luminosity: 'dark',
-			format: 'rgba',
-			alpha: 0.75,
-		})
-	);
+  const [cardColor, setCardColor] = useLocalStorage(
+    id,
+    randomColor({
+      luminosity: "dark",
+      format: "rgba",
+      alpha: 0.75,
+    })
+  );
 
-	async function getSingleCourse() {
-		const token = await currentUser.getIdToken();
-		const result = await fetch(
-			`${process.env.REACT_APP_URL}/classroom/course/read/?courseId=${id}`,
-			{
-				method: 'get',
-				headers: {
-					Authorization: `Bearer ${token}`,
-					'Content-type': 'application/json',
-				},
-			}
-		);
+  async function getSingleCourse() {
+    const token = await currentUser.getIdToken();
+    const result = await fetch(
+      `${process.env.REACT_APP_URL}/classroom/course/read/?courseId=${id}`,
+      {
+        method: "get",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json",
+        },
+      }
+    );
 
-		const body = await result.json();
-		console.log(body);
-		setCourse(body.data.course);
-		//console.log(body.data);
-	}
+    const body = await result.json();
+    console.log(body);
+    setCourse(body.data.course);
+    //console.log(body.data);
+  }
 
-	async function getAssignment() {
-		const token = await currentUser.getIdToken();
-		const result = await fetch(
-			`${process.env.REACT_APP_URL}/classroom/courses/courseWork?courseId=${id}`,
-			{
-				method: 'get',
-				headers: {
-					Authorization: `Bearer ${token}`,
-					'Content-type': 'application/json',
-				},
-			}
-		);
+  async function getAssignment() {
+    const token = await currentUser.getIdToken();
+    const result = await fetch(
+      `${process.env.REACT_APP_URL}/classroom/courses/courseWork?courseId=${id}`,
+      {
+        method: "get",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json",
+        },
+      }
+    );
 
-		const body = await result.json();
-		setCourseAssignment(body.data.courseWork);
-		console.log(body.data.courseWork);
-	}
+    const body = await result.json();
+    setCourseAssignment(body.data.courseWork);
+    console.log(body.data.courseWork);
+  }
 
-	useEffect(() => {
-		getAssignment();
-		if (props.location.state) setCourse(props.location.state.course);
-		else getSingleCourse();
-	}, [currentUser]);
+  async function checkMoss() {
+    const token = await currentUser.getIdToken();
+    const result = await fetch(`${process.env.REACT_APP_URL}/moss/id/read`, {
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-type": "application/json",
+      },
+    });
 
-	return (
-		<div>
-			<Navbar />
-			{course && (
-				<div className="assignmentBox">
-					<div className="courseDesc" style={{ backgroundColor: cardColor }}>
-						<div className="assignmentCourseDetails">
-							<div className="assignmentCourseName">{course.name}</div>
-							<div className="assignmentCourseSection">
-								{course.section ? course.section : ' '}
-							</div>
-							<div className="courseTeacher">{course.teacherInfo.name.fullName}</div>
-						</div>
-					</div>
-					<div className="assignmentCard">
-						{courseAssignment.map(assign => {
-							return <CourseAssignmentCard assignment={assign} />;
-						})}
-					</div>
-				</div>
-			)}
-		</div>
-	);
+    const body = await result.json();
+    body.data.mossId && setMossId(true);
+    console.log(body.data);
+  }
+  useEffect(() => {
+    getAssignment();
+    checkMoss();
+    if (props.location.state) setCourse(props.location.state.course);
+    else getSingleCourse();
+  }, [currentUser]);
+
+  return (
+    <div>
+      <Navbar />
+      {props.location.state.label == "teacher" ? (
+        !mossId ? (
+          <AddMossId />
+        ) : null
+      ) : null}
+      {course && (
+        <div className="assignmentBox">
+          <div className="courseDesc" style={{ backgroundColor: cardColor }}>
+            <div className="assignmentCourseDetails">
+              <div className="assignmentCourseName">{course.name}</div>
+              <div className="assignmentCourseSection">
+                {course.section ? course.section : " "}
+              </div>
+              <div className="courseTeacher">
+                {course.teacherInfo.name.fullName}
+              </div>
+            </div>
+          </div>
+          <div className="assignmentCard">
+            {courseAssignment.map((assign) => {
+              return <CourseAssignmentCard assignment={assign} />;
+            })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
