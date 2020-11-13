14b341f0d4c693734e756badbfbb78831b585f4d
Kushal Garg <kushalgarg2000@gmail.com>
2020-11-13 13:37:39 +0530

    Implements loaders

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
-import { useEffect,useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { useEffect, useContext } from 'react';
 import firebase from '../../utils/firebase';
 import { HomeIcon } from 'react-line-awesome';
-import { AuthContext } from "../../context/Auth";
+import { AuthContext } from '../../context/Auth';
 import './Sidebar.css';
 import SidebarElements from './components/SidebarElement/SidebarElements';
+import Loader from '../Loader/Loader';
 
 export default function Sidebar(props) {
 	const { currentUser, loading } = useContext(AuthContext);
 	const [teachingCourses, setTeachingCourses] = useState([]);
 	const [enrolledCourses, setEnrolledCourses] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
 	async function getCourses() {
 		const token = await props.currentUser.getIdToken();
@@ -25,6 +28,7 @@ export default function Sidebar(props) {
 		const body = await result.json();
 		setTeachingCourses(body.data?.courses?.teacher ?? []);
 		setEnrolledCourses(body.data?.courses?.student ?? []);
+		setIsLoading(false);
 	}
 
 	function logout() {
@@ -38,21 +42,18 @@ export default function Sidebar(props) {
 
 	async function unlinkDrive() {
 		const token = await currentUser.getIdToken();
-		const result = await fetch(
-		  `${process.env.REACT_APP_URL}/auth/unlink`,
-		  {
-			method: "post",
+		const result = await fetch(`${process.env.REACT_APP_URL}/auth/unlink`, {
+			method: 'post',
 			headers: {
-			  Authorization: `Bearer ${token}`,
-			  "Content-type": "application/json",
+				Authorization: `Bearer ${token}`,
+				'Content-type': 'application/json',
 			},
-		  }
-		);
-	
+		});
+
 		const body = await result.json();
 		console.log(body);
 		logout();
-	  }
+	}
 
 	useEffect(() => {
 		getCourses();
@@ -60,35 +61,43 @@ export default function Sidebar(props) {
 
 	return (
 		<aside className={props.open ? 'sidebar-active' : ''}>
-			<a href='/courses' className='allCourses'>
+			<Link to='/courses' className='allCourses'>
 				<div className='home'>
 					<HomeIcon />
 				</div>
 				Classes
-			</a>
-			{teachingCourses.length > 0 && (
-				<div>
-					<div className='sideHead'>Teaching</div>
+			</Link>
+			{isLoading ? (
+				<Loader />
+			) : (
+				<React.Fragment>
+					{teachingCourses.length > 0 && (
+						<div>
+							<div className='sideHead'>Teaching</div>
+							<div>
+								{teachingCourses.map(course => {
+									return <SidebarElements details={course} />;
+								})}
+							</div>
+						</div>
+					)}
+					{enrolledCourses.length > 0 && (
+						<div>
+							<div className='sideHead'>Enrolled</div>
+							<div>
+								{enrolledCourses.map(course => {
+									return <SidebarElements details={course} />;
+								})}
+							</div>
+						</div>
+					)}
 					<div>
-						{teachingCourses.map(course => {
-							return <SidebarElements details={course} />;
-						})}
+						<div className='unlink' onClick={unlinkDrive}>
+							Unlink Drive and Classroom
+						</div>
 					</div>
-				</div>
+				</React.Fragment>
 			)}
-			{enrolledCourses.length > 0 && (
-				<div>
-					<div className='sideHead'>Enrolled</div>
-					<div>
-						{enrolledCourses.map(course => {
-							return <SidebarElements details={course} />;
-						})}
-					</div>
-				</div>
-			)}
-			<div>
-				<div className="unlink" onClick={unlinkDrive}>Unlink Drive and Classroom</div>
-			</div>
 		</aside>
 	);
 }
